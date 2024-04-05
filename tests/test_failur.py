ee8938dfb7b60e7cc88580492880069bbc4f8f95
a923304 <mohammad-ali.raisi@eviden.com>
2024-04-05 19:00:28 +0200

    added skip and xfail

---
+++ tests/test_failur.py
@@ -0,0 +1,18 @@
+# Test Suite after N Test Failures
+# if we want to stop the the test suite after certain number of fails use
+  # pytest --maxfail = <num>
+
+import pytest
+import math
+
+def test_sqrt_failure():
+    num = 25
+    assert math.sqrt(num) == 6
+
+def test_square_failure():
+    num = 7
+    assert 7*7 == 40
+
+def test_quality_failure():
+    assert 10 == 11
+    
